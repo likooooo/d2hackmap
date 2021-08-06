44f3201d55d73b4cb6475e122816742f2742ef46
likooooo <462866046@qq.com>
2021-08-06 13:49:51 +0800

    借用公司的vs2008编译的结果，晚上测一下看能不能用

--- script/d2hackmap_Build.bat
+++ script/d2hackmap_Build.bat
@@ -1,4 +1,4 @@
 copy ..\cmake\CMakeLists.txt ..\build
 copy ..\cmake\d2hackmap.cmake ..\build
-cmake -G "Visual Studio 16 2019" -A Win32 ../build/CMakeLists.txt
-cmake --build ../build --config Release
+cmake -G "Visual Studio 9 2008" -A Win32 ../build/CMakeLists.txt
+cmake --build ../build 
